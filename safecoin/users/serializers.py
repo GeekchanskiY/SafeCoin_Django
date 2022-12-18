e23e9b6f7abaca7de807bbddd2771e9a3732b9bb
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-18 16:15:25 +0300

    user serializer added

--- safecoin/users/serializers.py
+++ safecoin/users/serializers.py
@@ -1,2 +1,9 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework import serializers
+from .models import SCUser
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        fields = ('id', 'username', 'email', 'birthday', 'about_me', 'avatar', )
+        model = SCUser
+        read_only_fields = ('id',)
