84ac9c889b0d7221f0c3e790165f892ade26d0ba
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 04:21:40 +0300

    fields added

--- safecoin/users/serializers.py
+++ safecoin/users/serializers.py
@@ -4,6 +4,6 @@ from .models import SCUser
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        fields = ('id', 'username', 'email', 'birthday', 'about_me', 'avatar', )
+        fields = ('id', 'username', 'email', 'birthday', 'about_me', 'avatar', 'country')
         model = SCUser
         read_only_fields = ('id',)
