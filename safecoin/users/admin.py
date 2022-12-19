7899033ede83a059444444717cf179a6faf5b711
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-19 04:21:34 +0300

    fields added

--- safecoin/users/admin.py
+++ safecoin/users/admin.py
@@ -4,8 +4,8 @@ from .models import SCUser
 
 @admin.register(SCUser)
 class UserAdmin(admin.ModelAdmin):
-    fields = ('username', 'email', 'birthday', 'about_me', 'avatar', 'subscribes', 'likes')
-    list_display = ('username', 'email')
-    search_fields = ('username', 'email')
+    fields = ('username', 'email', 'birthday', 'about_me', 'avatar', 'subscribes', 'likes', 'country')
+    list_display = ('email', 'username',)
+    search_fields = ('username', 'email', 'country')
     readonly_fields = ('email',)
     filter_vertical = ('subscribes', 'likes')
--- safecoin/users/models.py
+++ safecoin/users/models.py
@@ -8,10 +8,11 @@ class SCUser(AbstractUser):
     email = models.EmailField('email address', unique=True)
     birthday = models.DateField(null=True, blank=True)
     about_me = models.TextField(null=True, blank=True)
-    avatar = models.ImageField(upload_to='users/', null=True)
+    avatar = models.ImageField(upload_to='users/', null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
 
-    subscribes = models.ManyToManyField("cryptos.Crypto", related_name="sub_cryptos")
-    likes = models.ManyToManyField("cryptos.Crypto", related_name="like_cryptos")
+    subscribes = models.ManyToManyField("cryptos.Crypto", related_name="sub_cryptos", blank=True)
+    likes = models.ManyToManyField("cryptos.Crypto", related_name="like_cryptos", blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
